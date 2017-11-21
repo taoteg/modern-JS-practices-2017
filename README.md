84480eee5038fa310650192640e1349344492089
taoteg <taoteg@gmail.com>
2017-11-21 16:11:31 -0600

    Added npm to project.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # modern-JS-practices-2017
+
+https://medium.com/@peterxjang/modern-javascript-explained-for-dinosaurs-f695e9747b70
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // index.js
 console.log("Hello from JavaScript!");
+
 // Use moment.js.
 console.log(moment().startOf('day').fromNow());
